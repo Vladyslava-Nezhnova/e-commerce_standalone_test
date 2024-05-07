751566549e243bdfe74bfab55b261d7e0ac8d6b8
Vladyslava-Nezhnova <vldnezhnova@gmail.com>
2024-05-07 17:57:31 +0200

    create screenshot utility for catching failed tests

--- src/test/java/tests/SubmitOrderTest.java
+++ src/test/java/tests/SubmitOrderTest.java
@@ -3,11 +3,15 @@ package tests;
 import TestComponents.BaseTest;
 
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import vladyslavanezhnova.pageobjects.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -39,6 +43,13 @@ public class SubmitOrderTest extends BaseTest {
         ProductCatalogue productCatalogue = landingPage.loginApplication("vldnezhnova@gmail.com", "Testing2024");
         OrderPage orderPage = productCatalogue.goToOrdersPage();
         Assert.assertTrue(orderPage.VerifyOrderDisplay(productName));
+    }
+    public String getScreenshot(String testCaseName) throws IOException {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        File file = new File(System.getProperty("user.dir")+ "//reports//" + testCaseName+ ".png");
+        FileUtils.copyFile(source, file);
+        return System.getProperty("user.dir")+ "//reports//" + testCaseName+ ".png";
     }
     @DataProvider
     public Object[][] getData() throws IOException {
